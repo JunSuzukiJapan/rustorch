9c532c4d47a38c1ddf90e512a2d581f447489711
JunSuzukiJapan <JunSuzukiJapan@users.noreply.github.com>
2025-08-29 05:38:30 +0000

    deploy: 48826f3a9548e3b11ed7590f389f778d6fc7bdf2

--- rustorch/tensor/ops/sidebar-items.js
+++ rustorch/tensor/ops/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["arithmetic","matrix","signal","statistical","utilities"]};
+window.SIDEBAR_ITEMS = {"mod":["arithmetic","mathematical","matrix","signal","statistical","utilities"]};
