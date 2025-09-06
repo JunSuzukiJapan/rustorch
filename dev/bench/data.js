19b21e20d9effc1b40d6607fad89b404fa4b76d7
github-action-benchmark <github@users.noreply.github.com>
2025-09-06 02:48:27 +0000

    add RusTorch Benchmarks (customSmallerIsBetter) benchmark result for 5369f3bb5131e0edb8b0e4a7507d64d322486bee

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1757126907093,
+  "repoUrl": "https://github.com/JunSuzukiJapan/rustorch",
+  "entries": {
+    "RusTorch Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "jun.suzuki.japan@gmail.com",
+            "name": "JunSuzukiJapan",
+            "username": "JunSuzukiJapan"
+          },
+          "committer": {
+            "email": "jun.suzuki.japan@gmail.com",
+            "name": "JunSuzukiJapan",
+            "username": "JunSuzukiJapan"
+          },
+          "distinct": true,
+          "id": "5369f3bb5131e0edb8b0e4a7507d64d322486bee",
+          "message": "ignore .venv folder",
+          "timestamp": "2025-09-06T11:47:16+09:00",
+          "tree_id": "b54d04f2ac9fc97512dbae2affc624386bc5fd01",
+          "url": "https://github.com/JunSuzukiJapan/rustorch/commit/5369f3bb5131e0edb8b0e4a7507d64d322486bee"
+        },
+        "date": 1757126906180,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "RusTorch Quick Benchmark",
+            "value": 30015,
+            "unit": "ms",
+            "range": "±5%",
+            "extra": "Rust tensor operations benchmark"
+          }
+        ]
+      }
+    ]
+  }
+}
