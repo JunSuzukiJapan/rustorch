6ec0b7bb5d93eff6d71b73e26dfbb70558fe12f9
JunSuzukiJapan <JunSuzukiJapan@users.noreply.github.com>
2025-09-19 08:15:27 +0000

    deploy: cbf2984ebb00e28b43aa3375e0654d2b9f50db10

--- trait.impl/approx/relative_eq/trait.RelativeEq.js
+++ trait.impl/approx/relative_eq/trait.RelativeEq.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rustorch",[["impl&lt;T: <a class=\"trait\" href=\"https://docs.rs/num-traits/0.2/num_traits/float/trait.Float.html\" title=\"trait num_traits::float::Float\">Float</a> + RelativeEq&gt; RelativeEq for <a class=\"struct\" href=\"rustorch/tensor/complex_impl/core/struct.Complex.html\" title=\"struct rustorch::tensor::complex_impl::core::Complex\">Complex</a>&lt;T&gt;<div class=\"where\">where\n    T::Epsilon: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>,</div>"]]]]);
+    var implementors = Object.fromEntries([["rustorch",[["impl&lt;T: <a class=\"trait\" href=\"https://docs.rs/num-traits/0.2/num_traits/float/trait.Float.html\" title=\"trait num_traits::float::Float\">Float</a> + RelativeEq&gt; RelativeEq for <a class=\"struct\" href=\"rustorch/tensor/complex_impl/core/struct.Complex.html\" title=\"struct rustorch::tensor::complex_impl::core::Complex\">Complex</a>&lt;T&gt;<div class=\"where\">where\n    T::Epsilon: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rustorch",[["impl&lt;'a, T: <a class=\"trait\" href=\"https://docs.rs/num-traits/0.2/num_traits/float/trait.Float.html\" title=\"trait num_traits::float::Float\">Float</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"rustorch/distributed/data_parallel/struct.DistributedDataIterator.html\" title=\"struct rustorch::distributed::data_parallel::DistributedDataIterator\">DistributedDataIterator</a>&lt;'a, T&gt;"]]]]);
+    var implementors = Object.fromEntries([["rustorch",[["impl&lt;'a, T: <a class=\"trait\" href=\"https://docs.rs/num-traits/0.2/num_traits/float/trait.Float.html\" title=\"trait num_traits::float::Float\">Float</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"rustorch/distributed/data_parallel/struct.DistributedDataIterator.html\" title=\"struct rustorch::distributed::data_parallel::DistributedDataIterator\">DistributedDataIterator</a>&lt;'a, T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
